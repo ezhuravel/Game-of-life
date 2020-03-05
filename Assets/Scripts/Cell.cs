406038b5d915d845af29aa58c97f2b3b3b811804
ezhuravel <ezhuravel@gmail.com>
2020-03-04 23:14:26 -0500

    Set up basic "vision" with colliders

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    private bool alive = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,32 @@ public class Cell : MonoBehaviour
 
     void OnMouseDown()
     {
-        Debug.Log(gameObject.name);
+        var cellSprite = gameObject.GetComponent<SpriteRenderer>();
+        if (alive)
+        {
+            alive = false;
+            cellSprite.color = Color.white;
+        }
+        else
+        {
+            alive = true;
+            cellSprite.color = Color.red;
+        }
+
+
+        var collisions = Physics2D.OverlapBoxAll(gameObject.transform.position, new Vector2(2, 2), 0);
+
+        foreach (var collision in collisions)
+        {
+            if(collision.gameObject != gameObject)
+            {
+                SpriteRenderer spriteRenderer = null;
+
+                if (collision.TryGetComponent<SpriteRenderer>(out spriteRenderer))
+                {
+                    spriteRenderer.color = Color.green;
+                }
+            }           
+        }
     }
 }
