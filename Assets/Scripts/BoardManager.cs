210a1731b605d70af700aae2f257421c173ecabd
ezhuravel <ezhuravel@gmail.com>
2020-04-26 23:18:57 -0400

    Finished caves

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -11,24 +11,30 @@ public class BoardManager : MonoBehaviour
     public GameObject CellGameObject;
     public Camera Camera;
     public float GameSpeed = 1;
-    public int seed;
+    public bool WithCaves;
+    public int seed = 2;
+    public int fillPercent = 20;
     public bool paused = false;
     public Text StepText;
     public Text AliveCellText;
     public Text DeadCellText;
     public Text Ratio;
-    
+
     float elapsed = 0f;
    
     private int step = 0;
     private float ratio = 0;
     List<float> ratiosList;
-    private bool first = true;
-
+    private int[,] map;
+   
     void Start()
     {
-        //SetUpBoard();
-        SetUpBoardWithCaves();
+
+        if(WithCaves)
+            SetUpBoardWithCaves();
+        else
+            SetUpBoard();
+
         ratiosList = new List<float>();
 
         Camera.orthographicSize = Columns / 2 ;
@@ -41,7 +47,7 @@ public class BoardManager : MonoBehaviour
 
     private void SetUpBoard()
     {
-        System.Random r = new System.Random(2);
+        System.Random r = new System.Random(seed);
 
         for (int i = 0; i < Rows; i++)
         {
@@ -64,27 +70,14 @@ public class BoardManager : MonoBehaviour
 
     private void SetUpBoardWithCaves()
     {
-        RandomFillMap();       
-    }
+        System.Random r = new System.Random(seed);
+        map = new int[Rows, Columns];
+        RandomFillMap();
 
-    private void SmoothMap()
-    {
-        var cells = GetComponentsInChildren<TileBase>();
-        foreach (var cell in cells)
+        for (int i = 0; i < 5; i++)
         {
-            var tileScript = cell.GetComponent<TileBase>();
-            int neighbourWallTiles = tileScript.AdjacentBarrierCell();
-
-            if (neighbourWallTiles > 4)
-                tileScript.SetBarrier();
-            else if (neighbourWallTiles < 4)
-                tileScript.Die();
+            SmoothMap();
         }
-    }
-
-    private void RandomFillMap()
-    {
-        System.Random r = new System.Random(2);
 
         for (int i = 0; i < Rows; i++)
         {
@@ -95,26 +88,82 @@ public class BoardManager : MonoBehaviour
                 emptyTile.transform.position = new Vector2(j * TileSize, i * -TileSize);
 
                 var tileScript = emptyTile.GetComponent<TileBase>();
-                
-                var val = r.Next(2);
 
-                if (i == 0 || i == Rows - 1 || j == 0 || j == Rows - 1)
+                if(map[i,j] == 1)
                 {
                     tileScript.tileState = TileState.Barrier;
                 }
                 else
                 {
                     tileScript.tileState = TileState.Dead;
-                    if (r.Next(0, 100) < 45)
+
+                    var val = r.Next(2);
+                    if (val == 1)
+                        tileScript.tileState = TileState.Alive;
+                    else
+                        tileScript.tileState = TileState.Dead;
+                }
+            }
+        }
+    }
+
+    private void SmoothMap()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(i, j);
+
+                if (neighbourWallTiles > 4)
+                    map[i, j] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[i, j] = 0;
+
+            }
+        }
+    }
+
+    private int GetSurroundingWallCount(int i, int j)
+    {
+        int wallCount = 0;
+        for (int neighbourX = i - 1; neighbourX <= i + 1; neighbourX++)
+        {
+            for (int neighbourY = j - 1; neighbourY <= j + 1; neighbourY++)
+            {
+                if (neighbourX >= 0 && neighbourX < Rows && neighbourY >= 0 && neighbourY < Columns)
+                {
+                    if (neighbourX != i || neighbourY != j)
                     {
-                        tileScript.tileState = TileState.Barrier;
+                        wallCount += map[neighbourX, neighbourY];
                     }
                 }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
 
-                //if (val == 1)
-                //    tileScript.tileState = TileState.Alive;
-                //else
-                //    tileScript.tileState = TileState.Dead;
+        return wallCount;
+    }
+
+    private void RandomFillMap()
+    {
+        System.Random r = new System.Random(2);        
+
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Columns; j++)
+            {
+                if (i == 0 || i == Rows - 1 || j == 0 || j == Columns - 1)
+                {
+                    map[i, j] = 1;
+                }
+                else
+                {
+                    map[i, j] = (r.Next(0, 100) < fillPercent) ? 1 : 0;
+                }
             }
         }
     }
@@ -122,16 +171,6 @@ public class BoardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (first)
-        {
-            for (int i = 0; i < 0; i++)
-            {
-                SmoothMap();
-            }
-            first = false;
-        }
-
-
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -179,7 +218,7 @@ public class BoardManager : MonoBehaviour
 
             var totalCells = Rows * Columns;
             var liveCells = GetComponentsInChildren<TileBase>().Count(x => x.tileState == TileState.Alive);
-            var deadCells = totalCells - liveCells;
+            var deadCells = GetComponentsInChildren<TileBase>().Count(x => x.tileState == TileState.Dead);
 
             ratio = (float)liveCells / deadCells;
 
